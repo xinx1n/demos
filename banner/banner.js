5b6fb10e57bc5747854908802f3363d1450d26a9
xinx1n <xin9142@qq.com>
2017-04-21 15:56:36 +0800

    完成简单的 3D banner

---
+++ banner/banner.js
@@ -0,0 +1,13 @@
+let bannerNode = document.getElementById('banner')
+bannerNode.addEventListener('mousemove', function(e) {
+    let width = bannerNode.offsetWidth
+    let height = bannerNode.offsetHeight
+    let xDeg = (e.layerX - width / 2) / (width / 2) * 4
+    let yDeg = (e.layerY - height / 2) / (height / 2) * 12
+    bannerNode.style.transition = 'transform 0s ease-in-out'
+    bannerNode.style.transform = `translateZ(-100px) rotateX(${-yDeg}deg) rotateY(${xDeg}deg)`
+})
+bannerNode.addEventListener('mouseout', e => {
+    bannerNode.style.transition = 'transform 0.4s ease-in-out'
+    bannerNode.style.transform = `translateZ(-100px) rotateX(0deg) rotateY(0deg)`
+})
