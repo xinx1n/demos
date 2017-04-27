968bab6df392082b99fb015e0e5d801b69864f11
xinx1n <xin9142@qq.com>
2017-04-27 19:21:14 +0800

    update

--- banner/banner.js
+++ banner/banner.js
@@ -4,10 +4,13 @@ bannerNode.addEventListener('mousemove', function(e) {
     let height = bannerNode.offsetHeight
     let xDeg = (e.layerX - width / 2) / (width / 2) * 4
     let yDeg = (e.layerY - height / 2) / (height / 2) * 12
-    bannerNode.style.transition = 'transform 0s ease-in-out'
     bannerNode.style.transform = `translateZ(-100px) rotateX(${-yDeg}deg) rotateY(${xDeg}deg)`
 })
 bannerNode.addEventListener('mouseout', e => {
     bannerNode.style.transition = 'transform 0.4s ease-in-out'
     bannerNode.style.transform = `translateZ(-100px) rotateX(0deg) rotateY(0deg)`
+    setTimeout(function () {
+    	bannerNode.style.transition = 'transform 0s ease-in-out'
+    }, 1)
+
 })
