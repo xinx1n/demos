7c4f3ee2dfe77770e92a1677ede8621da5c17398
xinx1n <xin9142@qq.com>
2017-05-31 13:24:21 +0800

    将 sign/ HTML 中的 css 和 js 拆分出来

---
+++ sign/main.js
@@ -0,0 +1,50 @@
+var allInput = document.querySelectorAll('input:not([type="submit"])')
+Array.prototype.forEach.call(allInput, function(element, index) {
+    element.addEventListener('mousedown', function(e) {
+        console.log(3, 'mousedown');
+        let targetEl = e.target
+        let inputGroup = targetEl.closest('.input-group')
+        let line2 = inputGroup.children[2]
+        line2.style.cssText = `transform-origin: ${e.layerX}px center 0px; transform: scaleX(0); transition: all 0s;`
+    })
+    element.addEventListener('focus', function(e) {
+        let targetEl = e.target
+        let inputGroup = targetEl.closest('.input-group')
+        let line2 = inputGroup.children[2]
+        setTimeout(function() {
+            console.log(4, 'focus');
+            line2.style.transition = 'all .3s cubic-bezier(0.4,0,0.2,1)'
+            line2.style.transform = 'scale(1)'
+        }, 0)
+    })
+    element.addEventListener('blur', function(e) {
+        let targetEl = e.target
+        let inputGroup = targetEl.closest('.input-group')
+        let line2 = inputGroup.children[2]
+        line2.style.transform = 'scaleX(0)'
+    })
+    element.addEventListener('change', function(e) {
+        let targetEl = e.target
+        if (targetEl.value !== '') {
+            targetEl.classList.add('filled')
+        } else {
+            targetEl.classList.remove('filled')
+        }
+    })
+});
+
+
+// var myinput = document.getElementById('myinput')
+// myinput.onclick = function (e) {
+//  let inputGroup = e.target.closest('.input-group')
+//  let line2 = inputGroup.children[2]
+//  line2.style.cssText = `transform-origin: ${e.layerX}px center 0px;transform: scaleX(1);`
+//  setTimeout(function () {
+//      line2.style.transform = 'scale(1)'
+//  },1)
+// }
+// myinput.onblur = function (e) {
+//  let inputGroup = e.target.closest('.input-group')
+//  let line2 = inputGroup.children[2]
+//  line2.style.transform = 'scaleX(0)'
+// }
