f6135ef5bb43290550a61e0759dadfe7614e16e9
xinx1n <xin9142@qq.com>
2017-06-22 17:52:37 +0800

    cloudmusic 优化播放、暂停函数

--- cloudmusic/js/song.js
+++ cloudmusic/js/song.js
@@ -1,12 +1,10 @@
 $(function () {
 	var timer
-	var canPlay = false
 	// var mp3url = 'http://101.28.249.62/m10.music.126.net/20170622174535/767cba8a2cf4ce6b3c78f187402428eb/ymusic/3466/f88c/165c/3520e5664afd420989e88bc3a694c237.mp3?wshc_tag=1&wsts_tag=594b8be3&wsid_tag=dbe8c782&wsiphost=ipdbm'
 	var mp3url = 'http://omh8xg82p.bkt.clouddn.com/3520e5664afd420989e88bc3a694c237.mp3'
 	var audioEl = document.createElement('audio')
 	audioEl.src = mp3url	
 	$(audioEl).on('canplay',function () {
-		canPlay = true
 		audioEl.play()
 		$('.disc').addClass('playing')
 		bindBtnEve()
