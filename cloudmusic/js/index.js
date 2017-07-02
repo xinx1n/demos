8b1a83f8cf28fa1f34da590d429bb0ac75b725e8
xinx1n <xin9142@qq.com>
2017-07-02 16:37:16 +0800

    cloudmusic:完成index.html tab切换功能和推荐音乐这个tab

--- cloudmusic/js/index.js
+++ cloudmusic/js/index.js
@@ -6,18 +6,44 @@ $(function () {
 				dataType: 'json',
 			})
 			.done(function(data) {
-				console.log(data,'ss')
 				var elStr = ''
 				$.each(data,function (index,item) {
-					console.log(item)
-					elStr += '<li><a href="./song.html?id='+item.id+'">'+ item.name +'-'+item.singer+'</a></li>'
+					var malias = item.alias==''?'':'('+item.alias+')'
+					elStr += '<li> <a href="./song.html?id='+item.id+'"> <div class="info"> <div class="songtitle">'+item.name+'<span>'+malias+'</span></div> <div class="songinfo"> <svg class="sqimg"> <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-sq"></use> </svg> '+item.singer+' - '+item.album+' </div> </div> <div class="playimg"> <svg> <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-play-circled"></use> </svg> </div> </a> </li>'
+
 				})
-				console.log($(elStr))
-				$('#songs').append(elStr)
+				$('#lastestmusic').append(elStr)
+				$('#loadingmusic').remove()
 			})
 			.fail(function (error) {
 				console.log(error)
 			})
 	}
 	initData('./data/data.json')
+	$('.mtabnav').on('click','li.tabtitle',function (e) {
+		var $li = $(e.currentTarget)
+		var index = $li.index()
+		$li.addClass('active').siblings().removeClass('active')
+		$('.mtabcontent>li').removeClass('active').eq(index).addClass('active')
+		$li.trigger('tabChange', index)
+	})
+	$('.mtabnav').on('tabChange',function (e,index) {
+		var $li = $('.mtabcontent>li').eq(index)
+		if($li.attr('data-downloaded') === 'yes'){
+		      return 
+		}
+		if (index === 1) {
+			console.log('get2')
+	        $.get('./data/indextab2.json').then(function (response) {
+	          $li.text(response.msg.content)
+	          $li.attr('data-downloaded', 'yes')
+	        })
+	    }else if(index === 2){
+			console.log('get3')
+	        $.get('./data/indextab3.json').then(function (response) {
+	          $li.text(response.msg.content)
+	          $li.attr('data-downloaded', 'yes')
+	        })
+	    }
+	})
 })
